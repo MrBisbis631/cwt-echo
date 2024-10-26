3ce8daa6d84db36c0a31a3919898c7e8b698bb9b
MrBisbis631 <shmuelbisberg@gmail.com>
2024-10-26 23:47:58 +0300

    use controllers for comments and quotes

--- index.js
+++ index.js
@@ -1,74 +1,51 @@
 import express from "express";
-import { Comment, Quote } from "./models/index.js";
+import { CommentController } from "./controllers/CommentController.js";
+import { Comment } from "./models/index.js";
+import { QuoteController } from "./controllers/QuoteController.js";
 import { quoteRenderer, commentRenderer } from "./views/index.js";
 import bodyParser from "body-parser";
 
 const app = express();
 
+// middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Home page and list of top quotes
-app.get("/", async (_, res) => {
-  const quotes = await Quote.findAll({
-    order: [["createdAt", "DESC"]],
-  });
+// controllers
+const commentController = new CommentController();
+const quoteController = new QuoteController();
 
-  res.header("Content-Type", "text/html");
+// routes
+// all quotes
+app.get(
+  "/",
+  commentController.get(quoteRenderer, {}, { modelPayloadName: "quotes" })
+);
 
-  res.send(
-    quoteRenderer({
-      quotes,
-    })
-  );
-});
+//  see comments on one quote
+app.get(
+  "/:id",
+  quoteController.getOne(
+    commentRenderer,
+    {
+      include: [
+        {
+          model: Comment,
+          as: "comments",
+        },
+      ],
+    },
+    { modelPayloadName: "quote" }
+  )
+);
 
-// Page with a single quote and its comments
-app.get("/:id", async (req, res) => {
-  const quote = await Quote.findByPk(req.params.id, {
-    include: [
-      {
-        model: Comment,
-        as: "comments",
-      },
-    ],
-  });
+// create a new quote
+app.post("/quote", quoteController.post("/"));
 
-  if (!quote) {
-    res.status(404).send("Quote not found");
-    return;
-  }
-
-  res.header("Content-Type", "text/html");
-
-  res.send(
-    commentRenderer({
-      quote,
-    })
-  );
-});
-
-// Create a new quote
-app.post("/quote", async (req, res) => {
-  const data = req.body;
-
-  console.log(data);
-
-  await Quote.create(data);
-
-  res.redirect("/");
-});
-
-// Create a new comment on a quote
-app.post("/:id/comment", async (req, res) => {
-  const data = req.body;
-
-  await Comment.create({
-    quoteId: req.params.id,
-    text: data.text,
-  });
-
-  res.redirect(`/${req.params.id}`);
-});
+// create a new comment on a quote
+app.post(
+  "/:id/comment",
+  commentController.post((req, _) => `/${req.params.id}`)
+);
 
 // run
 app.listen(process.env.PORT || 3000, () => {
