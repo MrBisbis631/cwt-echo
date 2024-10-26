684c395e9bb5541624b5a103ec85273ffc5de0fd
MrBisbis631 <shmuelbisberg@gmail.com>
2024-10-26 23:47:32 +0300

    use dynamic database file path

--- models/index.js
+++ models/index.js
@@ -1,12 +1,14 @@
 import { Sequelize, DataTypes } from "sequelize";
 import QuoteBlueprint from "./quote.js";
 import CommentBlueprint from "./comment.js";
+import path from "path";
+
+const dbFilePath = path.join(path.resolve(), "database.sqlite");
 
 const connection = new Sequelize({
   dialect: "sqlite",
-  storage: "./database.sqlite",
+  storage: dbFilePath,
 });
-
 
 const Quote = QuoteBlueprint(connection, DataTypes);
 const Comment = CommentBlueprint(connection, DataTypes);
