856d7d49380b3a4fa091db4a575295ff7e515940
MrBisbis631 <shmuelbisberg@gmail.com>
2024-10-26 20:30:00 +0300

    create routes  for comments and quotes

--- index.js
+++ index.js
@@ -1,29 +1,73 @@
 import express from "express";
+import { Comment, Quote } from "./models/index.js";
+import { quoteRenderer, commentRenderer } from "./views/index.js";
+import bodyParser from "body-parser";
 
 const app = express();
 
-// middlewares
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// dev middlewares
-if (process.env.NODE_ENV === "development") {
-}
-
-// routes
 // Home page and list of top quotes
-app.get("/", (_, res) => {
-  res.send("Hi mom!");
+app.get("/", async (_, res) => {
+  const quotes = await Quote.findAll({
+    order: [["createdAt", "DESC"]],
+  });
+
+  res.header("Content-Type", "text/html");
+
+  res.send(
+    quoteRenderer({
+      quotes,
+    })
+  );
+});
+
+// Page with a single quote and its comments
+app.get("/:id", async (req, res) => {
+  const quote = await Quote.findByPk(req.params.id, {
+    include: [
+      {
+        model: Comment,
+        as: "comments",
+      },
+    ],
+  });
+
+  if (!quote) {
+    res.status(404).send("Quote not found");
+    return;
+  }
+
+  res.header("Content-Type", "text/html");
+
+  res.send(
+    commentRenderer({
+      quote,
+    })
+  );
 });
 
 // Create a new quote
-app.post("/quote", (req, res) => {
-  // Logic to create a new quote
-  res.send("New quote created!");
+app.post("/quote", async (req, res) => {
+  const data = req.body;
+
+  console.log(data);
+
+  await Quote.create(data);
+
+  res.redirect("/");
 });
 
 // Create a new comment on a quote
-app.post("/quote/:id/comment", (req, res) => {
-  // Logic to create a new comment on a quote
-  res.send(`New comment added to quote with id ${req.params.id}!`);
+app.post("/:id/comment", async (req, res) => {
+  const data = req.body;
+
+  await Comment.create({
+    quoteId: req.params.id,
+    text: data.text,
+  });
+
+  res.redirect(`/${req.params.id}`);
 });
 
 // run
